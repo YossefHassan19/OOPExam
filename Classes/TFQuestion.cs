9cddb2c7f418759c9fbfb067c37dc59c0820fc38
YossefHassan19 <yossef.hassan678@gmail.com>
2026-09-18 23:38:39 +0300

    TFQuestion

---
+++ Classes/TFQuestion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam.Classes
+{
+    internal class TFQuestion : Question
+    {
+        public TFQuestion(string header, string body, int mark, bool correctAnswerIsTrue) : base(header, body, mark)
+        {
+            answerList = new Answer[2]
+            {
+            new Answer(1, "True"),
+            new Answer(2, "False")
+            };
+            rightAnswer = correctAnswerIsTrue ? answerList[0] : answerList[1];
+        }
+        public override object Clone()
+        {
+            bool isTrue = rightAnswer.answerId == 1;
+            return new TFQuestion(header, body, mark, isTrue);
+        }
+
+        public override void showQuestion()
+        {
+            Console.WriteLine(this);
+            Console.Write(AnswersToString());
+        }
+    }
+}
