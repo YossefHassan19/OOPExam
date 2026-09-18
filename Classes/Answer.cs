76c9514b2e0efbcfcbaf2a9a307a0693660250c3
YossefHassan19 <yossef.hassan678@gmail.com>
2026-09-18 23:32:44 +0300

    Answer

---
+++ Classes/Answer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExam.Classes
+{
+    internal class Answer : ICloneable, IComparable<Answer>
+    {
+        public int answerId { get; set; }
+        public string answerText { get; set; }
+
+        public Answer(int answerId, string answerText)
+        {
+            this.answerId = answerId;
+            this.answerText = answerText;
+
+        }
+        public object Clone()
+        {
+            return new Answer(this.answerId, this.answerText);
+        }
+
+        public override string ToString()
+        {
+            return $"({answerId}) {answerText}";
+        }
+
+        public int CompareTo(Answer? other)
+        {
+            if (other == null) return 1;
+            return answerId.CompareTo(other.answerId);
+        }
+    }
+
+}
